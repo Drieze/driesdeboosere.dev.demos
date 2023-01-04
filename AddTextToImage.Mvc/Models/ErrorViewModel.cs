52dd2129fb3f6c7ad992c9ba54a5da2faeb8fbe9
Dries Deboosere <9217473+Drieze@users.noreply.github.com>
2023-01-04 15:39:17 +0100

    first commit

---
+++ AddTextToImage.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AddTextToImage.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
